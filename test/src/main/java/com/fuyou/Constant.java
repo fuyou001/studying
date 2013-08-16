65a009518280f9b47302963782a0bbed74c59deb
傅冲 <yubao.fyb@alibaba-inc.com>
2013-08-16 08:58:56 +0800

    添加统计文件单词代码

---
+++ test/src/main/java/com/fuyou/Constant.java
@@ -0,0 +1,11 @@
+package com.fuyou;
+
+/**
+ * User: yubao.fyb@alibaba-inc.com
+ * Date: 13-8-12
+ */
+public class Constant {
+    public static final String ENDLINE = "========================================";
+    public static final int ENDLINE_HASHCODE = ENDLINE.hashCode();
+
+}
