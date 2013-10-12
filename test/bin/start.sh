8b12a88ba425dd46a41586d8fd18b47adb262a63
傅冲 <yubao.fyb@alibaba-inc.com>
2013-10-12 16:21:24 +0800

    添加assembly的配置文件

---
+++ test/bin/start.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+cd `dirname $0`/..
+BASEDIR=`pwd`
+if [  -z $1 ];then
+  echo "fileNames is null...."
+  exit 1
+ fi
+fileNames=$1
+nohup java -server -cp $BASEDIR/lib/*:. notify.Main $fileNames > msg_test.log 2>&1 &
